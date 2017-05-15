7e6c579b864f33b61aee74b5b20af992ecfeb315
pswalt1 <pswalt1@C02-06>
2017-05-15 15:44:23 +0100

    Failure

--- Lemonade Tycoon/Log in.Designer.vb
+++ Lemonade Tycoon/Log in.Designer.vb
@@ -28,12 +28,14 @@ Partial Class Log_in
         Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.Label15 = New System.Windows.Forms.Label()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
         Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Panel1
         '
         Me.Panel1.BackColor = System.Drawing.Color.DodgerBlue
+        Me.Panel1.Controls.Add(Me.Button2)
         Me.Panel1.Controls.Add(Me.Button1)
         Me.Panel1.Controls.Add(Me.Label16)
         Me.Panel1.Controls.Add(Me.TextBox2)
@@ -46,44 +48,62 @@ Partial Class Log_in
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(74, 183)
+        Me.Button1.BackColor = System.Drawing.Color.White
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button1.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Button1.Location = New System.Drawing.Point(53, 178)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(147, 23)
+        Me.Button1.Size = New System.Drawing.Size(189, 23)
         Me.Button1.TabIndex = 48
-        Me.Button1.Text = "Sign in / Create account"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.Text = "Sign in"
+        Me.Button1.UseVisualStyleBackColor = False
         '
         'Label16
         '
         Me.Label16.AutoSize = True
-        Me.Label16.Location = New System.Drawing.Point(50, 110)
+        Me.Label16.Font = New System.Drawing.Font("Franklin Gothic Heavy", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label16.ForeColor = System.Drawing.Color.White
+        Me.Label16.Location = New System.Drawing.Point(52, 112)
         Me.Label16.Name = "Label16"
-        Me.Label16.Size = New System.Drawing.Size(56, 13)
+        Me.Label16.Size = New System.Drawing.Size(60, 15)
         Me.Label16.TabIndex = 47
         Me.Label16.Text = "Password:"
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(114, 107)
+        Me.TextBox2.Location = New System.Drawing.Point(53, 128)
         Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(128, 20)
+        Me.TextBox2.Size = New System.Drawing.Size(189, 20)
         Me.TextBox2.TabIndex = 46
         '
         'Label15
         '
         Me.Label15.AutoSize = True
-        Me.Label15.Location = New System.Drawing.Point(50, 62)
+        Me.Label15.Font = New System.Drawing.Font("Franklin Gothic Heavy", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label15.ForeColor = System.Drawing.Color.White
+        Me.Label15.Location = New System.Drawing.Point(50, 60)
         Me.Label15.Name = "Label15"
-        Me.Label15.Size = New System.Drawing.Size(58, 13)
+        Me.Label15.Size = New System.Drawing.Size(60, 15)
         Me.Label15.TabIndex = 45
         Me.Label15.Text = "Username:"
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(114, 55)
+        Me.TextBox1.Location = New System.Drawing.Point(53, 76)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(128, 20)
+        Me.TextBox1.Size = New System.Drawing.Size(189, 20)
         Me.TextBox1.TabIndex = 44
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.White
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button2.Location = New System.Drawing.Point(53, 207)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(189, 23)
+        Me.Button2.TabIndex = 49
+        Me.Button2.Text = "Sign up"
+        Me.Button2.UseVisualStyleBackColor = False
         '
         'Log_in
         '
@@ -105,4 +125,5 @@ Partial Class Log_in
     Friend WithEvents TextBox2 As TextBox
     Friend WithEvents Label15 As Label
     Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button2 As Button
 End Class
--- Lemonade Tycoon/Log in.vb
+++ Lemonade Tycoon/Log in.vb
@@ -3,7 +3,6 @@
         If TextBox1.Text = "" Or TextBox2.Text = "" Then
         Else
 
-
             For i As Integer = 1 To Database.usercount
                 If Database.database(i).username = TextBox1.Text And Database.database(i).password = TextBox2.Text Then
 
@@ -17,11 +16,6 @@
                     Form1.Show()
 
                 Else
-                    usercount += 1
-                    Database.database(usercount).username = TextBox1.Text
-                    Database.database(usercount).password = TextBox2.Text
-
-
                 End If
 
             Next
@@ -32,5 +26,29 @@
 
     Private Sub Log_in_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Database.read()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If TextBox1.Text = "" Or TextBox2.Text = "" Then
+        Else
+            Database.usercount += 1
+            Database.database(Database.usercount).username = TextBox1.Text
+            Database.database(Database.usercount).password = TextBox2.Text
+
+            Form1.Label4.Text = 0
+            Database.database(Database.usercount).lemons = 0
+            Form1.Label5.Text = 0
+            Database.database(Database.usercount).sugar = 0
+            Form1.Label7.Text = 0
+            Database.database(Database.usercount).ice = 0
+            Form1.Label12.Text = 0
+            Database.database(Database.usercount).profit = 0
+            Composition.totalsales = 0
+            Database.database(Database.usercount).customers = 0
+            Form1.Label2.Text = 1000
+            Database.database(Database.usercount).money = 1000
+            Me.Hide()
+            Form1.Show()
+        End If
     End Sub
 End Class
